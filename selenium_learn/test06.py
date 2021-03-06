896ca771ba538893cbf1706d5629b693af330a5d
liyihui <liyihuiid@qq.com>
2021-03-06 22:18:37 +0800

    在selenium脚本方法，下拉操作

---
+++ selenium_learn/test06.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+import time
+
+
+
+# 下拉操作selenium无法实现，只能依靠JavaScript
+driver = webdriver.Firefox()
+driver.get("http://ilxdh.com/")
+driver.find_element_by_css_selector(".close-btn").click()
+# 直接下拉到1000，下拉操作
+# js1 = "var q = document.documentElement.scrollTop=1000"
+# driver.execute_script(js1)
+# js = "window.scrollTo(0,1000)"
+# driver.execute_script(js)
+
+# 持续下拉
+for i in range(30):
+    js2 = "window.scrollTo(0, %s)"%(i * 100)
+    driver.execute_script(js2)
+    time.sleep(0.1)
+
+ 
